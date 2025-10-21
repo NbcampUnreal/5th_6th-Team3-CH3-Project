deb6fca9105ff0f9d3176330eb39cd989322c18a
khee8469 <bcw1992@naver.com>
2025-10-21 16:50:45 +0900

    create : PSMainMenuWidget Class,  PSMainMenuWidget Blueprint

---
+++ Source/ProjectSoul/Private/UI/PSMainMenuWidget.cpp
@@ -0,0 +1,26 @@
+#include "UI/PSMainMenuWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void UPSMainMenuWidget::NativeOnInitialized()
+{
+	TitleText = Cast<UTextBlock>(GetWidgetFromName(TEXT("TitleText")));
+	StartButton = Cast<UButton>(GetWidgetFromName(TEXT("StartButton")));
+	EndButton = Cast<UButton>(GetWidgetFromName(TEXT("EndButton")));
+
+	StartButton->OnClicked.AddDynamic(this, &UPSMainMenuWidget::StartButtonClick);
+	EndButton->OnClicked.AddDynamic(this, &UPSMainMenuWidget::EndButtonClick);
+}
+
+void UPSMainMenuWidget::StartButtonClick()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), "MainLevel");
+}
+
+void UPSMainMenuWidget::EndButtonClick()
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(), nullptr, EQuitPreference::Quit, false);
+}
+	
---
+++ Source/ProjectSoul/Public/UI/PSMainMenuWidget.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PSMainMenuWidget.generated.h"
+
+class UTextBlock;
+class UButton;
+
+UCLASS()
+class PROJECTSOUL_API UPSMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+private:
+	void StartButtonClick();
+	void EndButtonClick();
+
+public:
+
+protected:
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TitleText;
+	UPROPERTY(meta = (BindWidget))
+	UButton* StartButton;
+	UPROPERTY(meta = (BindWidget))
+	UButton* EndButton;
+
+};
