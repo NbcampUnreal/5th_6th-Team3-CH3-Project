b544fd98fd9e849f370eba7510c4b2145d4e2d52
yojung51123 <ak471125@naver.com>
2025-10-21 17:50:26 +0900

    Update WeaponBase

--- Source/ProjectSoul/Public/Weapon/PSWeaponBase.h
+++ Source/ProjectSoul/Public/Weapon/PSWeaponBase.h
@@ -10,12 +10,41 @@ class PROJECTSOUL_API APSWeaponBase : public AActor
 	GENERATED_BODY()
 	
 public:	
+
 	APSWeaponBase();
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+    float AttackPower;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+    float AttackRange;
+
+    UFUNCTION()
+    virtual void OnWeaponOverlap(
+        UPrimitiveComponent* OverlappedComp,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult);
+
+    virtual void EnableWeaponCollision();
+    virtual void DisableWeaponCollision();
+
+    // 데미지 전달 함수
+    static float ApplyDamage(
+        AActor* DamagedActor,
+        float BaseDamage,
+        AController* EventInstigator,
+        AActor* DamageCauser,
+        TSubclassOf<UDamageType> DamageTypeClass);
+
+	virtual void Tick(float DeltaTime) override;
+
 protected:
+
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void Tick(float DeltaTime) override;
+
+	
 
 };
