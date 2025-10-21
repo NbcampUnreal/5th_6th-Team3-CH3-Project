3d1313d31457616d7810a8c3a7e456c358f525f1
PDY <pdy127001@gmail.com>
2025-10-21 12:12:17 +0900

    Create GameModeBaseClass

---
+++ Source/ProjectSoul/Public/Gameplay/PSGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PSGameModeBase.generated.h"
+
+UCLASS()
+class PROJECTSOUL_API APSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
