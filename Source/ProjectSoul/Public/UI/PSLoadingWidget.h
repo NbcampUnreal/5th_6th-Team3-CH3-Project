d4ef27852c316d399e11192c57f3a2dee96c9ed4
khee8469 <bcw1992@naver.com>
2025-11-01 17:23:43 +0900

    create : LoadingWidget class

---
+++ Source/ProjectSoul/Public/UI/PSLoadingWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PSLoadingWidget.generated.h"
+
+UCLASS()
+class PROJECTSOUL_API UPSLoadingWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
